41ab76919d390ac25233e335457f4b55283fe9b1
kapilmehta098 <70791066+kapilmehta098@users.noreply.github.com>
2021-10-31 23:27:06 +0530

    Add files via upload

--- assign_three/array.s
+++ assign_three/array.s
@@ -1,14 +1,14 @@
-     AREA     largest, CODE, READONLY
+     AREA     largest_in_aray, CODE, READONLY
      EXPORT __main
      IMPORT printMsg
      ENTRY 
 __main  FUNCTION
 		MOV R1, #0x20000000
-		ADD R2,R1,#0x00000007
-		ADD R3,R2,#0x00000007
-		MOV R4, #0x1
-		MOV R5, #0x67
-		MOV R6, #0x90
+		ADD R2,R1,#0x00000005
+		ADD R3,R2,#0x00000005
+		MOV R4, #0x16
+		MOV R5, #0x17
+		MOV R6, #0x18
 		STR R4,[R1]
 		STR R5,[R2]   
 		STR R6,[R3]
